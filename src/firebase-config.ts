21d01f3a2e0a01c027848c50b2b59573c3b4b9f4
hhwork <hhwork@naver.com>
2025-10-01 01:21:24 +0900

    firebase config

---
+++ src/firebase-config.ts
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics , logEvent, type  Analytics} from "firebase/analytics";
+import { getFirestore, collection, addDoc, onSnapshot } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// Analytics (브라우저 환경에서만 동작)
+let analytics: Analytics | undefined;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
+
+const db = getFirestore(app);
+
+export {app,logEvent,analytics,db,collection,addDoc, onSnapshot}; 
